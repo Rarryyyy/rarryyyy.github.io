fed7df098d695a1c25f0665242e434912ee7eb2f
Rarryyyy <86099074+Rarryyyy@users.noreply.github.com>
2021-06-25 11:07:27 +0800

    login&register

---
+++ Code/JS/input.js
@@ -0,0 +1,32 @@
+let img = document.querySelectorAll(".img");
+let input = document.querySelectorAll(".input")
+
+function remove() {
+    for (let i=0;i<img.length;) {
+        img[i++].classList.remove('transform');
+    }
+    for (let i=0;i<input.length;) {
+        input[i++].classList.remove('transform');
+    }
+}
+function add() {
+    for (let i=0;i<img.length;) {
+        img[i++].classList.add('transform');
+    }
+    for (let i=0;i<input.length;) {
+        input[i++].classList.add('transform');
+    }
+}
+
+for (let i=0;i<input.length;i++) {
+    input[i].addEventListener('focus',function(){
+        if (img[i].classList.contains('transform')) {
+            remove();
+        } else {
+            add();
+        }
+    })
+    input[i].addEventListener('blur',function(){
+        remove();
+    })
+}
